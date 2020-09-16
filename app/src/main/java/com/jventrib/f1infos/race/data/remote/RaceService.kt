9db8e762093b623759365678b3aae3083f9051ad
jventrib <jventrib@gmail.com>
2020-09-17 00:08:51 +0200

    Remove Response in API calls Removed DataAccessStrategy.kt

--- app/src/main/java/com/jventrib/f1infos/race/data/remote/RaceService.kt
+++ app/src/main/java/com/jventrib/f1infos/race/data/remote/RaceService.kt
@@ -2,11 +2,12 @@ package com.jventrib.f1infos.race.data.remote
 
 import com.jventrib.f1infos.common.model.MRResponse
 import com.jventrib.f1infos.race.model.RaceTable
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RaceService {
     @GET("current.json")
-    suspend fun getRaces() : Response<MRResponse<RaceTable>>
+    suspend fun getRaces() : MRResponse<RaceTable>
 }
