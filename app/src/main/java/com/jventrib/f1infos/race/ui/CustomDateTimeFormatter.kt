51a41c832623d56954303d49c2f0d820c96e5e71
jventrib <jventrib@gmail.com>
2020-09-28 23:01:50 +0200

    Color tuning Date format and size

---
+++ app/src/main/java/com/jventrib/f1infos/race/ui/CustomDateTimeFormatter.kt
@@ -0,0 +1,65 @@
+package com.jventrib.f1infos.race.ui
+
+import java.time.chrono.IsoChronology
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.ResolverStyle
+import java.time.format.SignStyle
+import java.time.temporal.ChronoField
+import java.util.*
+
+
+val customDateTimeFormatter: DateTimeFormatter?
+    get() {
+
+
+        // manually code maps to ensure correct data always used
+        // (locale data can be changed by application code)
+        val dow: MutableMap<Long, String> = HashMap()
+        dow[1L] = "Mon"
+        dow[2L] = "Tue"
+        dow[3L] = "Wed"
+        dow[4L] = "Thu"
+        dow[5L] = "Fri"
+        dow[6L] = "Sat"
+        dow[7L] = "Sun"
+        val moy: MutableMap<Long, String> = HashMap()
+        moy[1L] = "Jan"
+        moy[2L] = "Feb"
+        moy[3L] = "Mar"
+        moy[4L] = "Apr"
+        moy[5L] = "May"
+        moy[6L] = "Jun"
+        moy[7L] = "Jul"
+        moy[8L] = "Aug"
+        moy[9L] = "Sep"
+        moy[10L] = "Oct"
+        moy[11L] = "Nov"
+        moy[12L] = "Dec"
+        val formatter = DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .parseLenient()
+            .optionalStart()
+            .appendText(ChronoField.DAY_OF_WEEK, dow)
+            .appendLiteral(", ")
+            .optionalEnd()
+            .appendValue(ChronoField.DAY_OF_MONTH, 1, 2, SignStyle.NOT_NEGATIVE)
+            .appendLiteral(' ')
+            .appendText(ChronoField.MONTH_OF_YEAR, moy)
+            .appendLiteral(' ')
+            .appendValue(ChronoField.YEAR, 4) // 2 digit year not handled
+            .appendLiteral(' ')
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+            .optionalEnd()
+            .appendLiteral(' ')
+            .toFormatter()
+
+        return formatter
+    }
+
+
