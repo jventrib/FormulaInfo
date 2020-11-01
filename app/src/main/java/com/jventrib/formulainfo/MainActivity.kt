d0fae8414e136bf9b7745fcbec3f831eaf91bc04
jventrib <jventrib@gmail.com>
2020-11-01 21:44:22 +0100

    Spinner theme

--- app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
+++ app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
@@ -3,7 +3,13 @@ package com.jventrib.formulainfo
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
+import com.jventrib.formulainfo.season.model.Season
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +22,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
+
+        val item: MenuItem = menu!!.findItem(R.id.spinner)
+        val spinner = MenuItemCompat.getActionView(item) as Spinner // get the spinner
+
+        spinner.adapter = ArrayAdapter(
+            applicationContext, R.layout.spinner_season, listOf(
+                Season(2017),
+                Season(2018),
+                Season(2019),
+                Season(2020)
+            )
+        ).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+
+        //spinner.onItemSelectedListener = onItemSelectedListener
+
         return true
     }
 }
---
+++ app/src/main/java/com/jventrib/formulainfo/season/model/Season.kt
@@ -0,0 +1,8 @@
+package com.jventrib.formulainfo.season.model
+
+data class Season(val year: Int){
+
+    override fun toString(): String {
+        return "$year"
+    }
+}
