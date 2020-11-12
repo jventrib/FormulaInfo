d1eec65d82b4e2553148318383474ee34d49c836
jventrib <jventrib@gmail.com>
2020-11-12 12:18:36 +0100

    Icon tuning Icon attribution in About Fragment

--- app/src/main/java/com/jventrib/formulainfo/about/AboutFragment.kt
+++ app/src/main/java/com/jventrib/formulainfo/about/AboutFragment.kt
@@ -1,6 +1,8 @@
 package com.jventrib.formulainfo.about
 
 import android.os.Bundle
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +17,13 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAboutBinding.inflate(inflater, container, false)
+        binding.textIcon.text = Html.fromHtml(
+            "<div>Icon made by <a href=\"https://www.flaticon.com/authors/freepik\" " +
+                    "title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" " +
+                    "title=\"Flaticon\">www.flaticon.com</a></div>",
+            Html.FROM_HTML_MODE_LEGACY
+        )
+        binding.textIcon.movementMethod = LinkMovementMethod.getInstance()
         return binding.root
     }
 }
