709f4e6b1c191692bd756c814a5b9be32ff69808
jventrib <jventrib@gmail.com>
2020-11-18 18:58:00 +0100

    MinSdk 21 Race Detail Layout sort

--- app/src/main/java/com/jventrib/formulainfo/about/AboutFragment.kt
+++ app/src/main/java/com/jventrib/formulainfo/about/AboutFragment.kt
@@ -1,5 +1,6 @@
 package com.jventrib.formulainfo.about
 
+import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.text.method.LinkMovementMethod
@@ -17,12 +18,17 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAboutBinding.inflate(inflater, container, false)
-        binding.textIcon.text = Html.fromHtml(
-            "<div>Icon made by <a href=\"https://www.flaticon.com/authors/freepik\" " +
-                    "title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" " +
-                    "title=\"Flaticon\">www.flaticon.com</a></div>",
-            Html.FROM_HTML_MODE_LEGACY
-        )
+        val s = "<div>Icon made by <a href=\"https://www.flaticon.com/authors/freepik\" " +
+                "title=\"Freepik\">Freepik</a> from <a href=\"https://www.flaticon.com/\" " +
+                "title=\"Flaticon\">www.flaticon.com</a></div>"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // FROM_HTML_MODE_LEGACY is the behaviour that was used for versions below android N
+            // we are using this flag to give a consistent behaviour
+            binding.textIcon.text = Html.fromHtml(s, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            binding.textIcon.text = Html.fromHtml(s);
+        }
         binding.textIcon.movementMethod = LinkMovementMethod.getInstance()
         return binding.root
     }
