ff15fd93aa53a3f650df13fdc60543ec5a1094e3
jventrib <jventrib@gmail.com>
2021-08-04 13:20:27 +0200

    experimental flow and store features warning disabled

--- app/src/main/java/com/jventrib/formulainfo/ComposeMainActivity.kt
+++ app/src/main/java/com/jventrib/formulainfo/ComposeMainActivity.kt
@@ -4,25 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
-import com.dropbox.android.external.store4.ExperimentalStoreApi
 import com.dropbox.android.external.store4.ResponseOrigin
 import com.dropbox.android.external.store4.StoreResponse
 import com.jventrib.formulainfo.race.model.db.Circuit
 import com.jventrib.formulainfo.race.model.db.Race
 import com.jventrib.formulainfo.race.model.db.RaceFull
 import com.jventrib.formulainfo.ui.theme.FormulaInfoTheme
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import java.time.Instant
 
-@ExperimentalStoreApi
-@ExperimentalCoroutinesApi
-@FlowPreview
 class ComposeMainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +27,22 @@ class ComposeMainActivity : ComponentActivity() {
         viewModel.setSeasonPosition(0)
         setContent {
             FormulaInfoTheme {
-                val raceList by viewModel.races.observeAsState(StoreResponse.Loading(ResponseOrigin.SourceOfTruth))
-                RaceList(raceList)
+                RaceScreen(viewModel)
             }
         }
+    }
+
+}
+
+@Composable
+private fun RaceScreen(viewModel: MainViewModel) {
+    Scaffold {
+        val raceList by viewModel.races.observeAsState(
+            StoreResponse.Loading(
+                ResponseOrigin.SourceOfTruth
+            )
+        )
+        RaceList(raceList)
     }
 }
 
@@ -55,6 +62,19 @@ fun RaceListPreview() {
     val sessions = Race.Sessions(race = Instant.now())
     val race = Race(2021, 1, "", "Race1", "Circuit1", sessions)
     val race2 = Race(2021, 2, "", "Race2", "Circuit1", sessions)
-    val circuit = Circuit("Circuit1", "url", "Circuit one", Circuit.Location(1.0f, 1.0f, "Fr", "France", "flag"), "url")
-    RaceList(raceList = StoreResponse.Data(listOf(RaceFull(race, circuit), RaceFull(race2, circuit)), ResponseOrigin.SourceOfTruth))
+    val circuit = Circuit(
+        "Circuit1",
+        "url",
+        "Circuit one",
+        Circuit.Location(1.0f, 1.0f, "Fr", "France", "flag"),
+        "url"
+    )
+    RaceList(
+        raceList = StoreResponse.Data(
+            listOf(
+                RaceFull(race, circuit),
+                RaceFull(race2, circuit)
+            ), ResponseOrigin.SourceOfTruth
+        )
+    )
 }
