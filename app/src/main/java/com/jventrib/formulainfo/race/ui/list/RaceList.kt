d070d450da895ecd5008432decced1e4310fd9bd
jventrib <jventrib@gmail.com>
2021-08-05 22:09:09 +0200

    fix RaceItem placeholder

--- app/src/main/java/com/jventrib/formulainfo/race/ui/list/RaceList.kt
+++ app/src/main/java/com/jventrib/formulainfo/race/ui/list/RaceList.kt
@@ -13,7 +13,6 @@ import com.dropbox.android.external.store4.ResponseOrigin
 import com.dropbox.android.external.store4.StoreResponse
 import com.jventrib.formulainfo.IMainViewModel
 import com.jventrib.formulainfo.MockMainViewModel
-import com.jventrib.formulainfo.R
 import com.jventrib.formulainfo.race.model.db.RaceFull
 import com.jventrib.formulainfo.race.ui.list.item.RaceItem
 import com.jventrib.formulainfo.ui.theme.FormulaInfoTheme
@@ -45,7 +44,7 @@ fun RaceList(raceList: StoreResponse<List<RaceFull>>) {
     LazyColumn {
         raceList.dataOrNull()?.let { raceList ->
             items(raceList) {
-                RaceItem(it, R.drawable.japan)
+                RaceItem(it)
             }
         }
     }
