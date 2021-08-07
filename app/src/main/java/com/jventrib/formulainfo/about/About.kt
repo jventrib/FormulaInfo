45866f38c2c65eb5fcf2b4d876097b07761c13cd
jventrib <jventrib@gmail.com>
2021-08-07 15:05:33 +0200

    About screen almost complete

--- app/src/main/java/com/jventrib/formulainfo/about/About.kt
+++ app/src/main/java/com/jventrib/formulainfo/about/About.kt
@@ -1,27 +1,119 @@
 package com.jventrib.formulainfo.about
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.jventrib.formulainfo.R
 
 @Composable
-@Preview
+@Preview(showSystemUi = true)
 fun About() {
-    Column(Modifier.fillMaxWidth()) {
-        Row {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             androidx.compose.foundation.Image(
-                painter = rememberImagePainter("", builder = {this.placeholder(R.mipmap.ic_launcher_round)}),
+                painter = rememberImagePainter(
+                    "",
+                    builder = { this.placeholder(R.mipmap.ic_launcher_round) }),
                 null
             )
-            Text(text = "Formula Info")
+            Column() {
+                Text(
+                    text = stringResource(R.string.about_title),
+                    style = MaterialTheme.typography.h4
+                )
+                Text(
+                    text = "0.0.1",
+                    style = MaterialTheme.typography.subtitle2,
+                    modifier = Modifier.align(End)
+                )
+
+            }
         }
+        B2(R.string.about_developed)
+        Text(
+            text = with(AnnotatedString.Builder()) {
+                append("link: Jetpack Compose")
+                // attach a string annotation that stores a URL to the text "Jetpack Compose".
+                addStringAnnotation(
+                    tag = "URL",
+                    annotation = "https://developer.android.com/jetpack/compose",
+                    start = 6,
+                    end = 21
+                )
+                toAnnotatedString()
+            },
+            style = MaterialTheme.typography.body2
+        )
+        B2(R.string.about_purpose)
+        H6(R.string.about_disclaimer_title)
+        B2(R.string.about_disclaimer)
+        H6(R.string.about_datasource_title)
 
+        Row {
+            S2(R.string.about_datasource_ergast_title)
+            B2(R.string.about_datasource_ergast)
+        }
+        Row {
+            S2(R.string.about_datasource_wikipedia_title)
+            B2(R.string.about_datasource_wikipedia)
+        }
+        Row {
+            S2(R.string.about_datasource_f1c_title)
+            B2(R.string.about_datasource_f1c)
+        }
     }
+}
 
+@Composable
+private fun H6(resId: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(resId),
+        style = MaterialTheme.typography.h6,
+        modifier = modifier.padding(top = 16.dp)
+    )
+}
+
+@Composable
+private fun B2(resId: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(resId),
+        style = MaterialTheme.typography.body2,
+        modifier = modifier.padding(top = 16.dp)
+    )
+}
+
+@Composable
+private fun RowScope.S2(resId: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(resId),
+        style = MaterialTheme.typography.subtitle2,
+        modifier = modifier
+            .padding(top = 16.dp, end = 8.dp)
+            .weight(.3f)
+    )
+}
+
+@Composable
+private fun RowScope.B2(resId: Int, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(resId),
+        style = MaterialTheme.typography.body2,
+        modifier = modifier
+            .padding(top = 16.dp)
+            .weight(.7f)
+    )
 }
