4bceb6457f090e49a7e6105e07b57762326c2fbd
jventribout <jventribout@smartwavesa.com>
2021-09-03 20:01:56 +0200

    landscapist for images

--- app/src/main/java/com/jventrib/formulainfo/common/ui/components/ItemCard.kt
+++ app/src/main/java/com/jventrib/formulainfo/common/ui/components/ItemCard.kt
@@ -8,14 +8,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jventrib.formulainfo.R
 import com.jventrib.formulainfo.ui.theme.LightLightGrey
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun ItemCard(
-    imageUrl: String?,
+    imageUrl: String,
     placeholder: Int,
     topText: String,
     bottomText: String,
@@ -32,7 +36,7 @@ fun ItemCard(
 
 @Composable
 fun ItemCard(
-    imageUrl: String?,
+    imageUrl: String,
     placeholder: Int,
     onItemSelected: () -> Unit,
     content: @Composable() (ColumnScope.() -> Unit)
