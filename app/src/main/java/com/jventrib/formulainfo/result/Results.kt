e8f61e3e2eb6b9809159a843aa7af3c879fce29d
jventribout <jventribout@smartwavesa.com>
2021-09-22 22:41:28 +0200

    Result Layout change DeltaText

--- app/src/main/java/com/jventrib/formulainfo/result/Results.kt
+++ app/src/main/java/com/jventrib/formulainfo/result/Results.kt
@@ -1,24 +1,24 @@
 package com.jventrib.formulainfo.result
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jventrib.formulainfo.common.ui.components.DeltaTextP
 import com.jventrib.formulainfo.race.model.db.Constructor
 import com.jventrib.formulainfo.race.model.db.Driver
 import com.jventrib.formulainfo.race.model.db.RaceResult
 import com.jventrib.formulainfo.race.model.db.RaceResultFull
 import com.jventrib.formulainfo.race.ui.components.ItemCard
-import java.time.Instant
 
 @Composable
 fun Results(results: List<RaceResultFull>) {
@@ -52,19 +52,28 @@ fun ResultItem(raceResult: RaceResultFull, onResultSelected: () -> Any) {
                     style = MaterialTheme.typography.body2
                 )
             }
-            Column(horizontalAlignment = Alignment.End, modifier = Modifier) {
+            Column(horizontalAlignment = End, modifier = Modifier.fillMaxWidth()) {
                 Text(
-                    text = "${raceResult.raceResult.points} pts",
-                    style = MaterialTheme.typography.h6
+                    text = "${raceResult.raceResult.points.toInt()} pts",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.align(End)
                 )
-                Text(
-                    text = raceResult.constructor.name,
-                    style = MaterialTheme.typography.body1
-                )
+                Row() {
+
+                    Text(
+                        text = "Started${raceResult.raceResult.grid}",
+                        style = MaterialTheme.typography.body1
+                    )
+                    DeltaTextP(
+                        delta = raceResult.raceResult.position - raceResult.raceResult.grid,
+                        Modifier.padding(start = 8.dp)
+                    )
+                }
             }
         }
     }
 }
+
 
 @Preview
 @Composable
@@ -80,7 +89,7 @@ fun ResultItemPreview() {
             25.0f,
             "verstappen",
             "RedBull",
-            1,
+            2,
             70,
             "Finished",
             RaceResult.Time(111, "111"),
