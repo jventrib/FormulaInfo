2b594690d09a1f32fcbc5d20366cad791918e108
jventrib <jventrib@gmail.com>
2021-10-12 22:39:11 +0200

    Android View version cleanup

--- app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
+++ app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.jventrib.formulainfo
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.jventrib.formulainfo.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            MainScreen()
+        }
     }
+
 }
+
+
+//@Preview
+//@Composable
+//fun RaceListPreview() {
+//    val sessions = Race.Sessions(race = Instant.now())
+//    val race = Race(2021, 1, "", "Race1", "Circuit1", sessions)
+//    val race2 = Race(2021, 2, "", "Race2", "Circuit1", sessions)
+//    val circuit = Circuit(
+//        "Circuit1",
+//        "url",
+//        "Circuit one",
+//        Circuit.Location(1.0f, 1.0f, "Fr", "France", "flag"),
+//        "url"
+//    )
+//    val list = listOf(
+//        RaceFull(race, circuit),
+//        RaceFull(race2, circuit)
+//    )
+//    RaceList(
+//        raceList = StoreResponse.Data(
+//            list, ResponseOrigin.SourceOfTruth
+//        )
+//    )
+//}
+
