1cde753e9eb19c9e497fa33ca7e3edcbee251580
jventrib <jventrib@gmail.com>
2021-10-25 21:05:23 +0200

    NestedScroll for RaceDetail cleanup

--- app/src/main/java/com/jventrib/formulainfo/ui/race/RaceDetail.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/race/RaceDetail.kt
@@ -44,17 +44,16 @@ fun RaceDetail(fullRace: FullRace, raceResults: List<FullRaceResult>) {
                 }
             )
         }) {
-
         val circuitHeight = 246.dp
-        val circuitHeightPx = with(LocalDensity.current) { circuitHeight.roundToPx().toFloat() }
         var circuitScrollHeightPx by remember { mutableStateOf(0f) }
+        val circuitHeightPx = with(LocalDensity.current) { circuitHeight.roundToPx().toFloat() }
         val nestedScrollConnection = remember {
             object : NestedScrollConnection {
                 override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
                     val delta = available.y
                     logcat(LogPriority.VERBOSE) { "delta $delta" }
-                    circuitScrollHeightPx += delta
-                    circuitScrollHeightPx = circuitScrollHeightPx.coerceIn(-circuitHeightPx, 0f)
+                    circuitScrollHeightPx =
+                        (circuitScrollHeightPx + delta).coerceIn(-circuitHeightPx, 0f)
                     return Offset.Zero
                 }
             }
@@ -66,32 +65,24 @@ fun RaceDetail(fullRace: FullRace, raceResults: List<FullRaceResult>) {
             )
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(circuitHeight)
-//                    .padding(top = 80.dp)
                     .offset(0.dp, with(LocalDensity.current) {
                         (circuitScrollHeightPx)
-//                            .coerceIn(-circuitHeightPx, 0f)
                             .roundToInt()
                             .toDp()
                     })
                     .background(Color.White)
-//                    .border(1.dp, Color.Red),
             ) {
                 Column {
-
-                    RaceItem(
-                        fullRace = fullRace,
-                    )
-
+                    RaceItem(fullRace = fullRace)
                     Image(
                         imageModel = fullRace.circuit.imageUrl,
                         contentScale = ContentScale.Fit,
-                        modifier = Modifier.fillMaxWidth().height(circuitHeight - 80.dp)
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(circuitHeight - 80.dp)
                     )
                 }
             }
-
         }
     }
 }
