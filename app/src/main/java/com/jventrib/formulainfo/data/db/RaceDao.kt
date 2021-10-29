7a49531cfae1567735fab5cac91c1853b8c09a3a
jventribout <jventribout@smartwavesa.com>
2021-10-29 21:51:22 +0200

    Dependencies cleanup

--- app/src/main/java/com/jventrib/formulainfo/data/db/RaceDao.kt
+++ app/src/main/java/com/jventrib/formulainfo/data/db/RaceDao.kt
@@ -1,9 +1,6 @@
 package com.jventrib.formulainfo.data.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.jventrib.formulainfo.model.db.Race
 import com.jventrib.formulainfo.model.db.RaceInfo
 import kotlinx.coroutines.flow.Flow
@@ -11,9 +8,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface RaceDao {
 
+    @Transaction
     @Query("SELECT * from race WHERE season = :season AND round = :round")
     fun getRace(season: Int, round: Int): Flow<Race>
 
+    @Transaction
     @Query("SELECT * from race WHERE season = :season ORDER BY round ASC")
     fun getRaces(season: Int): Flow<List<Race>>
 
--- app/src/main/java/com/jventrib/formulainfo/data/db/ResultDao.kt
+++ app/src/main/java/com/jventrib/formulainfo/data/db/ResultDao.kt
@@ -1,9 +1,6 @@
 package com.jventrib.formulainfo.data.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.jventrib.formulainfo.model.db.Result
 import com.jventrib.formulainfo.model.db.ResultInfo
 import kotlinx.coroutines.flow.Flow
@@ -11,9 +8,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ResultDao {
 
+    @Transaction
     @Query("SELECT * from race_result WHERE season = :season and round = :round and driverId = :driverId")
     fun getResult(season: Int, round: Int, driverId: String): Flow<Result>
 
+    @Transaction
     @Query("SELECT * from race_result WHERE season = :season and round = :round ORDER BY position ASC")
     fun getResults(season: Int, round: Int): Flow<List<Result>>
 
