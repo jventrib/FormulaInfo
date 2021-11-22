36edc54dc2551d2d63966f50b656cb53d626b655
jventrib <jventrib@gmail.com>
2021-11-22 18:37:19 +0100

    Chart drawPoints

--- app/src/main/java/com/jventrib/formulainfo/ui/results/Chart.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/results/Chart.kt
@@ -10,67 +10,61 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.material.math.MathUtils.lerp
+import kotlin.random.Random
 
+val intervals = floatArrayOf(20f, 20f)
+val pathEffect = PathEffect.dashPathEffect(intervals)
 
 @Composable
 fun <E> Chart(
     series: List<Serie<E>>,
     modifier: Modifier = Modifier,
-    valueBound: ValueBound? = null,
+    boundary: Boundary? = null,
 ) {
     var scrollOffset by remember { mutableStateOf(0f) }
     var scale by remember { mutableStateOf(2f) }
     var rotation by remember { mutableStateOf(0f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
     val transformState = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
-        scale *= zoomChange
+        scale = (scale * zoomChange).coerceAtLeast(1f)
         rotation += rotationChange
         offset += offsetChange
     }
-
-    fun scaleCoerced() = scale.coerceAtLeast(1f)
-
+    val scrollState = rememberScrollableState { delta ->
+        scrollOffset = scrollOffset.plus(delta / scale)
+        delta
+    }
 
     fun <E> DrawScope.drawSerie(
         serie: Serie<E>,
-        valueBound: ValueBound
+        boundary: Boundary
     ) {
         val seriePoints = serie.seriePoints
         val screenCenterX = size.width / 2f
-        fun scrollCoerced() = scrollOffset.coerceIn(
-            -screenCenterX + screenCenterX / scaleCoerced(),
-            screenCenterX - screenCenterX / scaleCoerced()
+        scrollOffset = scrollOffset.coerceIn(
+            -screenCenterX + screenCenterX / scale,
+            screenCenterX - screenCenterX / scale
         )
 
-        fun getElementXY(dataPoint: DataPoint<E>): Offset {
-            val xFraction = valueBound.run { (dataPoint.x - minX!!) / (maxX!! - minX) }
-            val yFraction = valueBound.run { (dataPoint.y - minY!!) / (maxY!! - minY) }
-            val lerp = lerp(-screenCenterX, screenCenterX, xFraction)
-            val x = (lerp + scrollOffset) * scaleCoerced() + screenCenterX
-            val y = lerp(0f, size.height, yFraction)
-            return Offset(x, y)
-        }
-        seriePoints.indices.forEach { index ->
-            val currentElement = seriePoints[index]
-            val nextElement = seriePoints.getOrNull(index + 1)
+        val screen = Rect(Offset.Zero - Offset(size.width * 1, 0f), size * 4f)
+        val points = seriePoints.map {
+            getElementXY(it, boundary, scrollOffset, scale)
+        }.filter { screen.contains(it) }
 
-            val currentOffset = getElementXY(currentElement)
-            nextElement?.let {
-                val nextOffset = getElementXY(nextElement)
-                drawLine(
-                    color = serie.color,
-                    start = currentOffset,
-                    end = nextOffset,
-                    strokeWidth = 3.dp.toPx()
-                )
-                drawCircle(color = serie.color, 10f, nextOffset)
-            }
-            drawCircle(color = serie.color, 10f, currentOffset)
-        }
+        drawPoints(
+            points,
+            PointMode.Polygon,
+            serie.color,
+            3.dp.toPx(),
+            StrokeCap.Round,
+        )
+        points.forEach { drawCircle(color = serie.color, 4.dp.toPx(), it) }
     }
 
 
@@ -81,20 +75,15 @@ fun <E> Chart(
                 .fillMaxHeight()
                 .padding(16.dp)
                 .border(1.dp, Color.Black)
-                .scrollable(
-                    state = rememberScrollableState { delta ->
-                        scrollOffset = scrollOffset.plus(delta / scaleCoerced())
-                        delta
-                    }, Orientation.Horizontal
-                )
+                .scrollable(scrollState, Orientation.Horizontal)
                 .transformable(transformState),
         ) {
-            val minX = valueBound?.minX ?: series.minOfOrNull { it.seriePoints.minOf { it.x } }
-            val maxX = valueBound?.maxX ?: series.maxOfOrNull { it.seriePoints.maxOf { it.x } }
-            val minY = valueBound?.minY ?: series.minOfOrNull { it.seriePoints.minOf { it.y } }
-            val maxY = valueBound?.maxY ?: series.maxOfOrNull { it.seriePoints.maxOf { it.y } }
+            val minX = boundary?.minX ?: series.minOfOrNull { it.seriePoints.minOf { it.x } }
+            val maxX = boundary?.maxX ?: series.maxOfOrNull { it.seriePoints.maxOf { it.x } }
+            val minY = boundary?.minY ?: series.minOfOrNull { it.seriePoints.minOf { it.y } }
+            val maxY = boundary?.maxY ?: series.maxOfOrNull { it.seriePoints.maxOf { it.y } }
 
-            val vb = ValueBound(minX, maxX, minY, maxY)
+            val vb = Boundary(minX, maxX, minY, maxY)
             series.forEach { serie ->
                 drawSerie(serie, vb)
             }
@@ -102,14 +91,55 @@ fun <E> Chart(
     }
 }
 
+
+fun <E> DrawScope.getElementXY(
+    dataPoint: DataPoint<E>,
+    boundary: Boundary,
+    scrollOffset: Float,
+    scale: Float
+): Offset {
+    val xFraction = boundary.run { (dataPoint.x - minX!!) / (maxX!! - minX) }
+    val yFraction = boundary.run { (dataPoint.y - minY!!) / (maxY!! - minY) }
+    val screenCenterX = size.width / 2f
+    val lerp = lerp(-screenCenterX, screenCenterX, xFraction)
+    val x = (lerp + scrollOffset) * scale + screenCenterX
+    val y = lerp(0f, size.height, yFraction)
+    return Offset(x, y)
+}
+
 data class Serie<E>(val seriePoints: List<DataPoint<E>>, val color: Color)
 
 data class DataPoint<E>(val x: Float, val y: Float, val element: E)
 
-data class ValueBound(
+data class Boundary(
     val minX: Float? = null,
     val maxX: Float? = null,
     val minY: Float? = null,
     val maxY: Float? = null
 )
 
+@Preview(showSystemUi = false)
+@Composable
+fun ChartPreview() {
+    val series = (0..5).map {
+        Serie(
+            (0..10).map {
+                DataPoint(it.toFloat(), Random.nextInt(20).toFloat(), "TEST")
+            }, Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat())
+        )
+    }
+
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .border(3.dp, Color.Blue)
+    ) {
+        Chart(
+            series = series,
+            modifier = Modifier
+                .fillMaxHeight(1f)
+                .border(2.dp, Color.Red),
+        )
+    }
+}
