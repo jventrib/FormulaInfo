55f8749a5a3d7550a8ed47a1a53dbde6ce8a3685
jventrib <jventrib@gmail.com>
2022-01-17 12:12:33 +0100

    Current Year as latest

--- app/src/main/java/com/jventrib/formulainfo/ui/schedule/SeasonViewModel.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/schedule/SeasonViewModel.kt
@@ -5,14 +5,17 @@ import com.dropbox.android.external.store4.StoreResponse
 import com.jventrib.formulainfo.data.RaceRepository
 import com.jventrib.formulainfo.model.db.Race
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.Instant
+import java.time.Year
+import java.time.temporal.TemporalField
 import javax.inject.Inject
 
 @HiltViewModel
 class SeasonViewModel @Inject constructor(private val repository: RaceRepository) : ViewModel() {
 
-    val seasonList = (1950..2021).toList().reversed()
+    val seasonList = (1950..Year.now().value).toList().reversed()
 
-    val season = MutableLiveData(2021)
+    val season = MutableLiveData(Year.now().value)
 
     val round: MutableLiveData<Int?> = MutableLiveData(null)
 
