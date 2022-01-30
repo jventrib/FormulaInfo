e775a2a092dd4e88c53f5b096cb6c7a80c0cd77e
jventrib <jventrib@gmail.com>
2022-01-30 18:15:30 +0100

    cleanup

--- app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
+++ app/src/main/java/com/jventrib/formulainfo/MainActivity.kt
@@ -22,29 +22,3 @@ class MainActivity : ComponentActivity() {
     }
 
 }
-
-
-//@Preview
-//@Composable
-//fun RaceListPreview() {
-//    val sessions = Race.Sessions(race = Instant.now())
-//    val race = Race(2021, 1, "", "Race1", "Circuit1", sessions)
-//    val race2 = Race(2021, 2, "", "Race2", "Circuit1", sessions)
-//    val circuit = Circuit(
-//        "Circuit1",
-//        "url",
-//        "Circuit one",
-//        Circuit.Location(1.0f, 1.0f, "Fr", "France", "flag"),
-//        "url"
-//    )
-//    val list = listOf(
-//        RaceFull(race, circuit),
-//        RaceFull(race2, circuit)
-//    )
-//    RaceList(
-//        raceList = StoreResponse.Data(
-//            list, ResponseOrigin.SourceOfTruth
-//        )
-//    )
-//}
-
