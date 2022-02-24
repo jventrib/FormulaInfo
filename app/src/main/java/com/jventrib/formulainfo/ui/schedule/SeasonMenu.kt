0ae61acb620d58a805b235cf42cc04d723d781ba
jventrib <jventrib@gmail.com>
2022-02-24 23:03:43 +0100

    ExposedDropDownMenu for season

--- app/src/main/java/com/jventrib/formulainfo/ui/schedule/SeasonMenu.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/schedule/SeasonMenu.kt
@@ -1,26 +1,28 @@
 package com.jventrib.formulainfo.ui.schedule
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.DropdownMenu
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
+import androidx.compose.material.ExposedDropdownMenuDefaults.TrailingIcon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jventrib.formulainfo.ui.theme.FormulaInfoTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SeasonMenu(
     seasonList: List<Int>,
@@ -29,33 +31,61 @@ fun SeasonMenu(
     onSeasonSelect: (Int) -> Unit
 ) {
     var expanded by remember { mutableStateOf(initiallyExpanded) }
-    Box(
-        Modifier
-            .clickable { expanded = !expanded }
-            .padding(8.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Text(selectedSeason.toString())
-            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = null)
-        }
-    }
-    DropdownMenu(
-        expanded = expanded,
-        onDismissRequest = { expanded = false }
-    ) {
-        seasonList.forEach { season ->
-            DropdownMenuItem(onClick = {
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = !expanded }) {
+        TextField(
+            readOnly = true,
+            value = "$selectedSeason",
+            onValueChange = {},
+            label = { Text("Season") },
+            trailingIcon = { TrailingIcon(expanded = expanded) },
+            modifier = Modifier.width(110.dp),
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(
+                focusedLabelColor = LocalContentColor.current,
+                unfocusedLabelColor = LocalContentColor.current,
+                trailingIconColor = LocalContentColor.current,
+                focusedTrailingIconColor = LocalContentColor.current,
+                unfocusedBorderColor = Color.Transparent,
+                focusedBorderColor = Color.Transparent
+            )
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
                 expanded = false
-                onSeasonSelect(season)
-            }) {
-                Text(
-                    season.toString(),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
-                )
+            }
+        ) {
+            seasonList.forEach { season ->
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        onSeasonSelect(season)
+                    }
+                ) {
+                    Text("$season")
+                }
             }
         }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SeasonMenuPreview() {
+    FormulaInfoTheme(true) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {},
+                    actions = {
+                        SeasonMenu(
+                            seasonList = (2022..2030).toList(),
+                            selectedSeason = 2022,
+                            onSeasonSelect = {},
+                            initiallyExpanded = true
+                        )
+                    }
+                )
+            }
+        ) {}
     }
 }
