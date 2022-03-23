623845605dcb5bba846962a831a89e24885ce6eb
jventribout <jventribout@smartwavesa.com>
2022-03-23 21:18:23 +0100

    Fix tests

--- app/src/test/java/com/jventrib/formulainfo/data/RaceRepositoryTest.kt
+++ app/src/test/java/com/jventrib/formulainfo/data/RaceRepositoryTest.kt
@@ -89,6 +89,7 @@ class RaceRepositoryTest : TestCase() {
                 now(),
                 now(),
                 now(),
+                now(),
                 now()
             )
         )
