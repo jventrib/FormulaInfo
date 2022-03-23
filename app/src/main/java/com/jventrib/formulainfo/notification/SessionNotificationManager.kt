4b7c029062a2c655362e9377affd38f568c72a0f
jventrib <jventrib@gmail.com>
2022-03-23 13:54:33 +0100

    Notification manager updates

--- app/src/main/java/com/jventrib/formulainfo/notification/SessionNotificationManager.kt
+++ app/src/main/java/com/jventrib/formulainfo/notification/SessionNotificationManager.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import com.jventrib.formulainfo.data.RaceRepository
+import com.jventrib.formulainfo.model.db.Session
 import com.jventrib.formulainfo.model.db.Session.FP1
 import com.jventrib.formulainfo.model.db.Session.FP2
 import com.jventrib.formulainfo.model.db.Session.FP3
@@ -13,11 +14,13 @@ import com.jventrib.formulainfo.model.db.Session.QUAL
 import com.jventrib.formulainfo.model.db.Session.RACE
 import com.jventrib.formulainfo.ui.common.format
 import com.jventrib.formulainfo.ui.preferences.PreferencesKeys
+import com.jventrib.formulainfo.ui.preferences.PreferencesKeys.notifyBefore
 import com.jventrib.formulainfo.ui.preferences.dataStore
+import com.jventrib.formulainfo.utils.currentYear
+import com.jventrib.formulainfo.utils.now
 import dagger.hilt.android.qualifiers.ApplicationContext
 import de.schnettler.datastore.manager.DataStoreManager
 import java.time.Instant
-import java.time.Year
 import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -30,13 +33,20 @@ class SessionNotificationManager @Inject constructor(
     private val repository: RaceRepository
 ) {
     suspend fun notifyNextRaces() {
+        val dataStoreManager = DataStoreManager(context.dataStore)
+        val notifyBefore: Float = dataStoreManager.getPreference(PreferencesKeys.notifyBefore)
+
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val nextRace =
-            repository.getRaces(Year.now().value, false).first().firstOrNull { it.nextRace }
+            repository.getRaces(currentYear(), false).first()
+                .firstOrNull {
+                    it.raceInfo.sessions.race.minus(notifyBefore.toLong(), ChronoUnit.MINUTES)
+                        .isAfter(now())
+                }
         nextRace?.let { race ->
 
             // Get next session
-            val sessions = race.raceInfo.sessions.run {
+            val sessions: Map<Instant?, Session> = race.raceInfo.sessions.run {
                 mapOf(
                     fp1 to FP1,
                     fp2 to FP2,
@@ -46,15 +56,12 @@ class SessionNotificationManager @Inject constructor(
                     this.race to RACE,
                 )
             }
-            val dataStoreManager = DataStoreManager(context.dataStore)
-            val notifyBefore: Float = dataStoreManager.getPreference(PreferencesKeys.notifyBefore)
             val nextSession =
                 sessions.entries
                     .firstOrNull {
                         it.key
                             ?.minus(notifyBefore.toLong(), ChronoUnit.MINUTES)
-                            ?.plusSeconds(10)
-                            ?.isAfter(Instant.now()) ?: false
+                            ?.isAfter(now()) ?: false
                     }
                     ?.toPair()
                     ?.let { it.first!! to it.second }
@@ -74,7 +81,7 @@ class SessionNotificationManager @Inject constructor(
                     context,
                     0,
                     intent,
-                    PendingIntent.FLAG_IMMUTABLE
+                    PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_CANCEL_CURRENT
                 )
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     alarmManager.setExactAndAllowWhileIdle(
