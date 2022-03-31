90e6f383cd1ab80071c9319e725f48f4a1a1d4d8
jventrib <jventrib@gmail.com>
2022-03-31 13:39:39 +0200

    Notification session switch handling reschedule notifications when preference update

--- app/src/main/java/com/jventrib/formulainfo/ui/preferences/PreferencesScreen.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/preferences/PreferencesScreen.kt
@@ -6,14 +6,21 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.jventrib.formulainfo.notification.calcNotifyBefore
 import com.jventrib.formulainfo.ui.common.toDurationString
 import de.schnettler.datastore.compose.material.PreferenceScreen
 import de.schnettler.datastore.compose.material.model.Preference
+import de.schnettler.datastore.manager.DataStoreManager
+import kotlinx.coroutines.flow.collect
+import logcat.logcat
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -21,11 +28,17 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "se
 @Composable
 fun PreferencesScreen() {
 
+    val preferencesViewModel: PreferencesViewModel = hiltViewModel()
+
     val dataStore = LocalContext.current.dataStore
-    // val dataStoreManager = remember { DataStoreManager(dataStore) }
-    // val scope = rememberCoroutineScope()
-    // val switchPreference by dataStoreManager.getPreferenceFlow(waterRequest)
-    //     .collectAsState(initial = false)
+    val dataStoreManager = remember { DataStoreManager(dataStore) }
+
+    LaunchedEffect(Unit) {
+        dataStoreManager.preferenceFlow.collect {
+            preferencesViewModel.sessionNotificationManager.notifyNextRaces()
+            logcat { "Rescheduling notifications" }
+        }
+    }
 
     Scaffold(
         topBar = { TopAppBar(title = { Text("Preferences") }) }
@@ -38,33 +51,33 @@ fun PreferencesScreen() {
                         Preference.PreferenceItem.SwitchPreference(
                             PreferencesKeys.notifyFP,
                             "Free Practice",
-                            summary = "Notify Free Practice",
+                            summary = "",
                             singleLineTitle = true,
                             icon = {}
                         ),
                         Preference.PreferenceItem.SwitchPreference(
                             PreferencesKeys.notifyQual,
                             "Qualification",
-                            summary = "Notify Qualification",
+                            summary = "",
                             singleLineTitle = true,
                             icon = {}
                         ),
                         Preference.PreferenceItem.SwitchPreference(
                             PreferencesKeys.notifyRace,
                             "Race",
-                            summary = "Notify Race",
+                            summary = "",
                             singleLineTitle = true,
                             icon = {}
                         ),
                         Preference.PreferenceItem.SeekBarPreference(
                             PreferencesKeys.notifyBefore,
                             "Notify Before",
-                            summary = "Minutes",
+                            summary = "",
                             singleLineTitle = true,
                             icon = {},
-                            valueRange = 0f..120f,
-                            steps = 121,
-                            valueRepresentation = { it.toLong().toDurationString() }
+                            valueRange = 0f..24f,
+                            steps = 24,
+                            valueRepresentation = { it.calcNotifyBefore().toDurationString() }
                         ),
                     )
                 )
--- app/src/main/java/com/jventrib/formulainfo/ui/preferences/PreferencesViewModel.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/preferences/PreferencesViewModel.kt
@@ -1,8 +1,10 @@
 package com.jventrib.formulainfo.ui.preferences
 
 import androidx.lifecycle.ViewModel
+import com.jventrib.formulainfo.notification.SessionNotificationManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-// @HiltViewModel
-class PreferencesViewModel : ViewModel() {
-    // val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-}
+@HiltViewModel
+class PreferencesViewModel @Inject constructor(val sessionNotificationManager: SessionNotificationManager) :
+    ViewModel()
