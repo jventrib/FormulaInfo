32da66c1c2c37a10c3181df75672db594ff4f0f1
jventrib <jventrib@gmail.com>
2022-05-25 18:13:11 +0200

    Fix Spotless

--- app/src/main/java/com/jventrib/formulainfo/ui/standing/DriverStandingChart.kt
+++ app/src/main/java/com/jventrib/formulainfo/ui/standing/DriverStandingChart.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateMap
 import androidx.compose.ui.Modifier
